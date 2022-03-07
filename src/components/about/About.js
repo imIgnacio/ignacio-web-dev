dca6c06da109adbf12579b5e16e5cd4e60bfc62f
Ignacio <nachodiaz99@gmail.com>
2022-03-08 02:15:36 +0300

    starting about

---
+++ src/components/about/About.js
@@ -0,0 +1,9 @@
+import './About.css';
+
+function About() {
+    return <section className="about">
+        <h2>Quienes Somos</h2>
+    </section>
+}
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import Header from '../components/header/Header';
 import Jumbo from '../components/jumbo/Jumbo';
 import Services from '../components/services/Services';
+import About from '../components/about/About';
 
 function Home() {
     return <div>
         <Header/>
         <Jumbo/>
         <Services/>
+        <About/>
     </div>
 }
 
