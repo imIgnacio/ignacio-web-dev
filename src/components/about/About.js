e2c35daccca118487be8d4049aa489698d6bb87d
Ignacio <nachodiaz99@gmail.com>
2022-03-08 22:29:06 +0300

    about done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,11 @@ import './About.css';
 
 function About() {
     return <section className="about">
-        <h2>Quienes Somos</h2>
+        <h2>Sobre Ignacio Web Dev</h2>
+        <p>Ignacio Diaz es un desarrollador Web Full-Stack, apasionado por crear y proporcionar lo solicitado por clientes.
+            Comprometido con la atención efectiva a todas las PYMES de chile, siempre esforzándome para entregar satisfacción al cliente.
+            Trabajando en conjunto para lograr los resultados esperados en el plazo estimado.
+        </p>
     </section>
 }
 
