7f93f77450bf6af50cc1459cc7952011caae9130
Ignacio <nachodiaz99@gmail.com>
2022-03-09 18:55:59 +0300

    header modified

--- src/components/jumbo/Jumbo.js
+++ src/components/jumbo/Jumbo.js
@@ -2,7 +2,7 @@ import './Jumbo.css'
 
 function Jumbo() {
     return <section className='jumbotron'>
-        <h2>Creamos la Web que necesitas</h2>
+        <h2 className='semi-title'>Creamos la Web que necesitas</h2>
         <hr/>
         <p className='paragraph'>
             Creamos la página o aplicación web que tú y tu PYME necesita, nosotros nos encargamos de todo. Diseño, hosting, dominio web, deployment, apliaciones web, SEO y más
