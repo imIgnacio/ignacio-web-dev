7b01cf0ef684ad5ec751eaf98e66c95a752aad5f
Ignacio <nachodiaz99@gmail.com>
2022-03-10 19:29:57 +0300

    ipad display done

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,12 +8,13 @@ import './Contact.css';
 
 function Contact() {
 
-    return <div className="contact">
+    return <section className="contact">
         <h2 id='contact'>Contáctame hoy</h2>
-        <WhatsApp link='https://wa.me/61448712130' logo={whatsAppIcon} text='+61448712130'/>
-        <Mail link='mailto:ignacio.diaz1299@gmail.com?subject=Desarrollo web' logo={mailIcon} text='Escríbeme'/>
-
-    </div>
+        <div className='icons'>
+            <WhatsApp link='https://wa.me/61448712130' logo={whatsAppIcon} text='+61448712130'/>
+            <Mail link='mailto:ignacio.diaz1299@gmail.com?subject=Desarrollo web' logo={mailIcon} text='Escríbeme'/>
+        </div>
+    </section>
 }
 
 export default Contact;
